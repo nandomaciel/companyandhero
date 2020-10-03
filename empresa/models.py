86e218af206f17ff67b3109e228f77f6582b3427
Fernando Rodrigues Maciel <fernandomaciel@ufrn.edu.br>
2020-10-03 09:14:22 -0300

    cnpj is unique

--- empresa/models.py
+++ empresa/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Empresa(models.Model):
-    nome = models.CharField(max_length=50)
-    cnpj = models.CharField(max_length=100)
+    nome = models.CharField('Nome', max_length=50)
+    cnpj = models.CharField('CNPJ', max_length=100, unique=True)
 
     def __str__(self):
         return self.nome
