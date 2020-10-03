66d03c1b31a0c5f289da06681114a7df2b823c40
Fernando Rodrigues Maciel <fernandomaciel@ufrn.edu.br>
2020-10-03 08:49:53 -0300

    remove docs

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,33 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
+
+API_TITLE = "API Company and Hero "
+API_DESCRIPTION = "Empresas e Funcionarios"
+
+admin.AdminSite.site_header = "Company and Hero"
+admin.AdminSite.site_title = "Company"
+admin.AdminSite.index_title = "Company and Hero"
+
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Company and Hero",
+#         default_version='v1',
+#         description="Teste",
+#         terms_of_service="https://companyandhero.heroku.com",
+#         contact=openapi.Contact(email="nandormaciel@gmail.com"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('empresa.urls')),
     path('api/v1/', include('funcionario.urls')),
+    # path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
