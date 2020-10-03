66984da40c2f9de08b675a08b87776528e6338fd
Fernando Rodrigues Maciel <fernandomaciel@ufrn.edu.br>
2020-10-03 07:58:47 -0300

    admin funcionarios

--- funcionario/admin.py
+++ funcionario/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-
 from .models import Funcionario
 
-admin.site.register(Funcionario)
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = (
+        'nome',
+        'username',
+        'get_empresas',
+    )
+    
+    fieldsets = (
+        ('Informações do funcionario', {'fields': ('nome', 'username')}),
+        ('Informações das empresas', {'fields': ('empresas',)})
+    )
